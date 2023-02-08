1873665533a1a82b57c154e196fb6742321bfa36
Jaewon Kim <macimaro04@gmail.com>
2023-02-08 15:59:19 +0900

    Feat: 기준 날짜 입력 시, 남은 '일 시간 분 초' 출력

--- d-day-counter/script.js
+++ d-day-counter/script.js
@@ -1,13 +1,23 @@
 const dateFormMaker = function () {
-    const inputYear = document.querySelector('#target-year-input');
-    const inputMonth = document.querySelector('#target-month-input');
-    const inputDate = document.querySelector('#target-date-input');
+    const inputYear = document.querySelector('#target-year-input').value;
+    const inputMonth = document.querySelector('#target-month-input').value;
+    const inputDate = document.querySelector('#target-date-input').value;
+
+    const dateFormat = `${inputYear}-${inputMonth}-${inputDate}`;
+    return dateFormat;
 };
 
 const counterMaker = function () {
-    const nowDate = new Date();
-    const targetDate = new Date('2024-01-01');
-    const remaining = ~~((targetDate - nowDate) / 1000)  // '~~': Math.floor()보다 빠름
+    const targetDateInput = dateFormMaker();
 
-    console.log(remaining);
+    const nowDate = new Date();
+    const targetDate = new Date(targetDateInput).setHours(0, 0, 0, 0);  // 자정 기준으로
+    const remaining = (targetDate - nowDate) / 1000  // '~~': Math.floor()보다 빠름
+
+    const remainingDate = Math.floor(remaining / 3600 / 24);
+    const remainingHours = Math.floor((remaining / 3600) % 24);
+    const remainingMinutes = Math.floor(remaining / 60) % 60;
+    const remainingSeconds = Math.floor(remaining) % 60;
+
+    console.log(remainingDate, remainingHours, remainingMinutes, remainingSeconds);
 }
