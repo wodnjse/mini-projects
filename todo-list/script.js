6f9f319426d4a7ba3f08e0bc2d6f63c6e94a6f26
wodnjse <macimaro04@gmail.com>
2023-04-21 23:59:59 +0900

    Feat: Store list in localStorage

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,24 +1,39 @@
 const todoInput = document.querySelector('#todo-input');
+const todoList = document.querySelector('#todo-list');
 
-const createTodo = function() {
-    const todoList = document.querySelector('#todo-list');
+const savedTodoList = JSON.parse(localStorage.getItem('saved-items'));
+
+
+const createTodo = function(storageData) {
+    let = todoContents = todoInput.value;
+    if(storageData) {
+        todoContents = storageData.contents;
+    } 
+    
     const newLi = document.createElement('li');
     const newSpan = document.createElement('span');
     const newBtn = document.createElement('button');
-
+    
     newBtn.addEventListener('click', () => {
         newLi.classList.toggle('complete');
+        saveItemsFn();
     });
-
+    
     newLi.addEventListener('dblclick', () => {
         newLi.remove();
+        saveItemsFn();
     });
+
+    if(storageData?.complete) {  // ?. : 옵셔널 체이닝
+        newLi.classList.add('complete');
+    }
     
     newSpan.textContent = todoInput.value;
     newLi.appendChild(newBtn);
     newLi.appendChild(newSpan);
     todoList.appendChild(newLi);
     todoInput.value = '';
+    saveItemsFn();
 }
 
 const keyCodeCheck = function() {
@@ -31,5 +46,29 @@ const deleteAll = function() {
     const liList = document.querySelectorAll('li');
     for(let i = 0; i < liList.length; i++) {
         liList[i].remove();
+    }
+    saveItemsFn();
+}
+
+const saveItemsFn = function() {
+    const saveItems = [];
+    for(let i = 0; i < todoList.children.length; i++) { 
+        const todoObj = {
+            contents : todoList.children[i].querySelector('span').textContent, 
+            complete : todoList.children[i].classList.contains('complete')
+        }
+        saveItems.push(todoObj);
+    }
+    
+    if(saveItems.length === 0) {
+        localStorage.removeItem('saved-items');
+    } else {
+        localStorage.setItem('save-items', JSON.stringify(saveItems));
+    }
+}
+
+if(savedTodoList) {
+    for(let i = 0; i < savedTodoList.length; i++) {
+        createTodo(savedTodoList[i]);
     }
 }
